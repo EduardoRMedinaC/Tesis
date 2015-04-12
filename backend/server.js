ce6239df34bad4cd050cf5f266d6fcd700df5ff0
marcelodavid <marcelo.arevalos@hotmail.com>
2015-04-11 23:26:29 -0400

    connect socketio client and insert perfil document to hogares collection

--- backend/server.js
+++ backend/server.js
@@ -13,6 +13,21 @@ var url = 'mongodb://localhost:27017/data';
 mongoclient.connect(url, {server: {poolSize: 3}}, function(err, db){
 	assert.equal(err, null, ['No pudo conectarse a la base de datos']);
 	console.log('correctamente conectado con la base de datos');
+
+	//conexion con el socket cliente
+	io.on('connection', function(client){
+		console.log("conexion establecido con el cliente");
+
+		//se recibe el perfil de la casa
+		client.on('perfil', function(data){
+			insertDocument(db, 'hogares', data);
+		});
+
+		db.collection('hogares').find({}).toArray(function(err, docs){
+			console.log(docs);
+		}); 
+	});
+	
 });
 
 server.listen(80, function(){
