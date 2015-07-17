74b186c2819862dd2bde093255fec61e0f6d7c3b
marcelodavid <marcelo.arevalos@hotmail.com>
2015-07-17 02:14:46 -0400

    Functions prototypes and constants

--- Psoc_Creator/Data Exchange MT174/Interface_driver.cydsn/main.c
+++ Psoc_Creator/Data Exchange MT174/Interface_driver.cydsn/main.c
@@ -1,15 +1,38 @@
 /* ========================================
  *
- * Copyright YOUR COMPANY, THE YEAR
+ * Copyright FP UNA, 2015
  * All Rights Reserved
  * UNPUBLISHED, LICENSED SOFTWARE.
  *
  * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF your company.
+ * WHICH IS THE PROPERTY OF FP UNA.
  *
  * ========================================
 */
 #include <project.h>
+#include <stdio.h>
+#include <string.h>
+
+/*******************************************************************************
+* Function Prototypes
+*******************************************************************************/
+static void BaudRate(char8 baudios);
+static void send(char8 bytes[], float tr);
+static int read_datablock(char8* data);
+static void readlineCR(char8* line, int select);
+static void substr(char8* substring, char8* string, int start, int end);
+
+
+
+/* Constants */
+#define ENABLED         (1u)
+#define DISABLED        (0u)
+#define NON_APPLICABLE  (DISABLED)
+
+/* RX and TX buffers UART operation */
+#define COMMON_BUFFER_SIZE     (16u)
+uint8 bufferTx[COMMON_BUFFER_SIZE];
+uint8 bufferRx[COMMON_BUFFER_SIZE + 1u];
 
 int main()
 {
