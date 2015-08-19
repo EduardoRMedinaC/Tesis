c36d239fd080f34abe34f8eb2a1cdef48c3e0fc2
marcelodavid <marcelo.arevalos@hotmail.com>
2015-08-19 17:37:25 -0400

    erase gCtrl controller, bind lat and lng parameter between the view and controller

--- public/js/google-maps-angular-api.js
+++ public/js/google-maps-angular-api.js
@@ -2,120 +2,138 @@
 	var app = angular.module('googleMaps',[]);
 
 	google.maps.event.addDomListener(window, 'load', function(){
-		app.directive('googleMapView', function($timeout){
-			return {
-				restrict: 'E',
-				templateUrl: 'templates/maps.html',
-				controller: function(){
-
-					this.options = {
-				    center:new google.maps
-				    	.LatLng(-25.25463261974944, -57.513427734375),
-				    zoom:6,
-				    mapTypeId:google.maps.MapTypeId.ROADMAP
-					};
-
-					this.map = new google.maps
-						.Map(document.getElementById("googleMap"),this.options);	
-
-					this.image = 'images/map-marker.png';
-
-					this.marker = new google.maps.Marker({
-			            title: 'Location',
-			            map: this.map,
-			            icon: this.image,
-			            draggable: true
-					});
-				},
-				controllerAs: 'gCtrl',
-				require: '?ngModel',
-				link: function(scope, element, attrs, ngModel) {
-
-					var counter = counter || 0,
-						gCtrl = scope.gCtrl;
-
-					google.maps.event
-						.addListener(gCtrl.map, 'click', function(event){
-							counter++;
-							gCtrl.centerCircle = new google
-							.maps.LatLng(event.latLng.k, event.latLng.D);
-
-							//permite graficar un circulo como máximo
-
-							if(counter <= 1){
-
-								//renderiza el valor inroducido por el usuario
-
-								ngModel.$render = function(){
-
-									//actualizamos el modelo desde la vista
-									
-									var newValue = gCtrl.circle.radius;
-
-									$timeout(function(){
-										gCtrl.circle.setRadius(+newValue);
-									});
-
-								};
-						  
-					        	gCtrl.circle = new google.maps.Circle({
-					        		center: gCtrl.centerCircle,
-						            map:gCtrl.map,
-						            clickable: false,
-						            // metres
-						            radius: 50000,
-						            fillColor: '#cfd8dc',
-						            fillOpacity: .6,
-						            strokeColor: '#3f51b5',
-						            strokeOpacity: .5,
-						            strokeWeight: 2,
-						            editable: true,
-					        	});
-
-					        	//actualizamos la vista desde el modelo
-					        	scope.$apply();
-
-					        }
-					        else {
-					        	gCtrl.circle.setCenter(
-					        		gCtrl.centerCircle
-					        	);
-					        };
-
-				        	google.maps.event
-					        	.addListener(
-					        		gCtrl.circle, 'radius_changed', function(){
-					        			scope.$apply();
-
-										//resfull 
-										$timeout(function(){
-						        			scope.restCtrl.GET(
-												gCtrl.circle.radius,
-												gCtrl.circle.center.k,
-												gCtrl.circle.center.D
-											);
-						        		});
-					        		}
-					        	);
-
-					        google.maps.event
-					        	.addListener(
-					        		gCtrl.circle,'center_changed', function(){
-						        		scope.$apply();
-
-						        		//resfull 
-						        		$timeout(function(){
-						        			scope.restCtrl.GET(
-												gCtrl.circle.radius,
-												gCtrl.circle.center.k,
-												gCtrl.circle.center.D
-											);
-						        		});
-					        		}
-					        	);
-				        });
-				}
-			};
-		});
+		console.log("arbol DOM totalmente construido");
+		angular.bootstrap(document, ['geoMeter']);
 	});
+	app.directive('googleMapView',['$timeout', function($timeout){
+		return {
+			restrict: 'E',
+			templateUrl: 'templates/maps.html',
+			require: '?ngModel',
+			link: function(scope, element, attrs, ngModel) {
+
+				var _counter = _counter || 0,
+					_image = 'images/map-marker.png';
+
+				scope.options = {
+			    center:new google.maps
+			    	.LatLng(-25.25463261974944, -57.513427734375),
+			    zoom:6,
+			    mapTypeId:google.maps.MapTypeId.ROADMAP
+				};
+
+				scope.map = new google.maps
+					.Map(document.getElementById("googleMap"),scope.options);
+
+				scope.marker = new google.maps.Marker({
+		            title: 'Location',
+		            map: scope.map,
+		            icon: _image,
+		            draggable: true
+				});
+
+				google.maps.event.addListener(scope.map, 'click', function(event){
+					_counter++;
+					scope.lat = event.latLng.lat();
+					scope.lng = event.latLng.lng();
+
+					// creamos un objeto LatLng
+					scope.LatLng = new google
+					.maps.LatLng(scope.lat, scope.lng);
+
+					// permite graficar un circulo como máximo
+					if(_counter <= 1){
+
+						// renderiza el valor inroducido por el usuario
+						ngModel.$render = function(){
+
+							//actualizamos el controlador desde la vista
+							var _newRadius = scope.circle.radius;
+
+							$timeout(function(){
+								scope.circle.setRadius(+_newRadius);
+							});
+
+						};
+				  
+			        	scope.circle = new google.maps.Circle({
+			        		center: scope.LatLng,
+				            map:scope.map,
+				            clickable: false,
+				            // metres
+				            radius: 50000,
+				            fillColor: '#cfd8dc',
+				            fillOpacity: .6,
+				            strokeColor: '#3f51b5',
+				            strokeOpacity: .5,
+				            strokeWeight: 2,
+				            editable: true,
+			        	});
+
+			        	// actualizamos la vista desde el controlador
+			        	scope.$apply();
+
+			        }
+			        else {
+			        	scope.circle.setCenter(
+			        		scope.LatLng
+			        	);
+			        };
+
+		        	google.maps.event.addListener(
+		        		scope.circle, 'radius_changed', function(){
+		        			scope.$apply();
+
+							//resfull 
+							$timeout(function(){
+			        			scope.restCtrl.GET(
+									scope.circle.radius,
+									scope.circle.center.lat,
+									scope.circle.center.lng
+								);
+			        		});
+		        		}
+		        	);
+
+			        google.maps.event.addListener(scope.circle,'center_changed', function(){
+			        	scope.$apply();
+
+			        	//resfull 
+			        	$timeout(function(){
+			        		scope.restCtrl.GET(
+								scope.circle.radius,
+								scope.lat,
+								scope.lng
+							);
+			        	});
+		        	});
+			    });
+			}
+		};
+	}]);
+
+	app.directive('latLng',[ '$timeout',function($timeout){
+		return {
+			restrict: 'A',
+			require: '?ngModel',
+			link: function(scope, element, attrs, ngModel){
+				ngModel.$render = function(){
+					if(ngModel.$viewValue)
+						element.val(ngModel.$viewValue);
+				};
+
+				ngModel.$commitViewValue = function(){
+					if(ngModel.$viewValue){
+						if(attrs.coord == "latitud")
+							scope.LatLng = new google.maps.LatLng(ngModel.$viewValue, scope.lng);
+						else
+							scope.LatLng = new google.maps.LatLng(scope.lat, ngModel.$viewValue);
+						$timeout(function(){
+							scope.circle.setCenter(scope.LatLng);
+						});
+					};
+				};
+			}
+		};
+	}]);
 })();
